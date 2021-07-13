e2dc60b37638eae5d7a0ea329ea2d96c726a26f2
wanoffski666 <petl-danila@mail.ru>
2021-07-13 13:06:32 +0300

    Initial Commit

---
+++ checkLists/checkListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkListsApp.swift
+//  checkLists
+//
+//  Created by Дане4ка on 13.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct checkListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
